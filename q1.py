96eb4e62de6d4249d99f132feacc4c98bb428468
mariarsouza <111761854+mariarsouza@users.noreply.github.com>
2022-12-05 08:37:11 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -22,9 +22,12 @@
 # Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
 
 def q1(heights):
-    
-    return heights.count(max(heights))
-
-
+    alturas = [180, 166, 170, 180]
+    max = None
+    for num in alturas:
+        if (max is None or num > max):
+        max = num
+    print('Número de alturas máximas:', alturas.count(max))
+    return 0
 if __name__ == '__main__':
     print(q1([180, 166, 170, 180]))
