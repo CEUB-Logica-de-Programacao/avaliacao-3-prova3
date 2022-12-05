48071d48836ac9703ae9e7ab0666b2d55094dc9a
Solino8 <111749902+Solino8@users.noreply.github.com>
2022-12-05 08:42:56 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -26,14 +26,16 @@
 #
 # Para obter a nota máxima dessa questão, não deve-se utilizar nenhuma função pronta do Python.
 
-def q4(s):
-    x = 0
-    for i in range(1, len(s)):
-        if s[i] == s[i].upper():
-            x +=1
-    if len(s) > 0:
-        x += 1
-    return x
-
+def q4(str): 
+    words = [[str[0]]] 
+  
+    for c in str[1:]: 
+        if words[-1][-1].islower() and c.isupper(): 
+            words.append(list(c)) 
+        else: 
+            words[-1].append(c) 
+  
+    return [''.join(word) for word in words] 
+    
 if __name__ == '__main__':
-    print(q4('hexaVemEsseAno'))
+    print(len(q4('hexaVemEsseAno')))
